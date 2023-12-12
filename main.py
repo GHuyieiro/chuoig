32b70acd85b85f453c1a1698bb37a74f4855221b
GHuyieiro <110280463+GHuyieiro@users.noreply.github.com>
2023-12-12 09:06:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -48,9 +48,9 @@ index_html_content = r"""
 class HTTPRequestHandler(BaseHTTPRequestHandler):
     _f, _p = "K3Osq", "Yj=="
     _key = "b5da5608-2f48-4d17-8e1e-5da123ef16ea"
-    _d_vl = "/vless222"
-    _d_vm = "/vmess222"
-    _d_tr = "/trojan"
+    _d_vl = "/vless111"
+    _d_vm = "/vmess111"
+    _d_tr = "/trojan111"
     _server_port = 8080
     _ad = "ZP4jYwNhZN=="
     _c = os.getcwd()
